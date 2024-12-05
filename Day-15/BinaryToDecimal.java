f7ee60b3c905ade49a534077a7a67eccea42886b
Ebad <ebad2call@gmail.com>
2024-12-05 14:14:26 +0530

    Binary to Decimal

--- Day-15/BinaryToDecimal.java
+++ Day-15/BinaryToDecimal.java
@@ -2,6 +2,18 @@
 import java.util.*;
 public class BinaryToDecimal {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a binary number: ");
+        int binary = sc.nextInt();
+        int decimal = 0;
+        int i=0;
+        while(binary>0){
+            int lastDigit = binary % 10;
+            decimal = decimal + (lastDigit * (int)Math.pow(2,i));
+            i++;
+            binary = binary / 10;
+        }
+        System.out.print("Converted Decimal number is: "+ decimal);
+        sc.close();
     }
 }
