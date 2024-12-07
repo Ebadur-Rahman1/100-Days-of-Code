c5866f05fa5d4105c6debc9217ccd88795f3d027
Ebad <ebad2call@gmail.com>
2024-12-08 03:58:02 +0530

    Inverted Half Pyramid

---
+++ Day-17/InvertedHalfPyramid.java
@@ -0,0 +1,21 @@
+// Print Inverted Half Pyramid Pattern using *.
+// ****
+// ***
+// **
+// *
+
+public class InvertedHalfPyramid {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1; j<=5; j++){
+                if(j<=5-i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
