1b920c87d20325b98c16f01340a9700f576b1e5e
Ebad <ebad2call@gmail.com>
2024-12-17 13:31:21 +0530

    Class name changed

--- Day-04/printNumber.java
+++ Day-04/printNumber.java
@@ -1,6 +1,6 @@
 // Write a program to print number from 1 to 10 using while loop.
 
-public class printNumber {
+public class PrintNumber {
     public static void main(String args[]){
      System.out.println("Numbers from 1 to 10: ");
      int number = 1;
