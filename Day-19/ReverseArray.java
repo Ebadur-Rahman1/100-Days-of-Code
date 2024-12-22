8cb354a725fc4a1fce6d82b04268db00798973a2
Ebad <ebad2call@gmail.com>
2024-12-22 18:44:48 +0530

    Day-19/ReverseArray.java

--- Day-19/ReverseArray.java
+++ Day-19/ReverseArray.java
@@ -1,7 +1,11 @@
 // Reverse an array with the help of second array.
 
 public class ReverseArray {
+    public static void reverseArray(){
+
+    }
     public static void main(String args[]){
         int arr[] = {2,4,6,8,10};
+        
     }    
 }
