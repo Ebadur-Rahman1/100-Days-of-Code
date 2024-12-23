a0c31acf65a5c50bb3b3716d95e8896b5b8a9c8b
Ebad <ebad2call@gmail.com>
2024-12-24 00:30:19 +0530

    updated

--- Day-19/ReverseArray2.java
+++ Day-19/ReverseArray2.java
@@ -1,6 +1,8 @@
 // Reverse an array using swaping technique for better space and time complexity.
-
-public class ReverseArray2 {
+    public class ReverseArray2 {
+    public static void reverseArray(int arr[]){
+        
+    }
     public static void main(String args[]){
         int arr[] = {2,4,6,8,10,12};
     }
