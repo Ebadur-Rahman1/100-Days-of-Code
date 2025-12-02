80806ef899364dc3d25875393b66c85fc31bfccf
Ebad <ebad2call@gmail.com>
2025-12-03 03:42:58 +0530

    Prefix sub array sum

--- Day-24/PrintSubArraySumTwo.java
+++ Day-24/PrintSubArraySumTwo.java
@@ -6,7 +6,32 @@
 
 
 public class PrintSubArraySumTwo {
-    public static void main(String[] args){
+    public static int prefixSubArraySum(int[] arr){
+        int maxSum = Integer.MIN_VALUE;
+        int prefixArray[] = new int[arr.length];
+        prefixArray[0] = arr[0];
+        int sum = 0;
+
+        for(int i=1; i<arr.length; i++){
+            prefixArray[i] = prefixArray[i-1] + arr[i];
+        }
+
+        for(int j=0; j<arr.length; j++){
+            int start = j;
+            for(int k=j; k<arr.length; k++){
+                int end = k;
+                sum = start == 0 ? prefixArray[end] : prefixArray[end] - prefixArray[start-1];
+                if(sum>maxSum){
+                    maxSum = sum;
+                }
+            }
+        }
         
+        return maxSum;
+    }
+    public static void main(String[] args){
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        int result = prefixSubArraySum(arr);
+        System.out.println(result);
     }
 }
